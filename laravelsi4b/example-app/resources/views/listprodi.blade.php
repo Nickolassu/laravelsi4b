82324fd365e5b7714f5bda1bf72db7a95b793a1f
Nickolassu <nickolassu_2226240047@mhs.mdp.ac.id>
2024-05-21 17:02:16 +0700

    NewUpdate

--- laravelsi4b/example-app/resources/views/listprodi.blade.php
+++ laravelsi4b/example-app/resources/views/listprodi.blade.php
@@ -1,3 +1,8 @@
+@extends('layout.main')
+
+@section('title','Prodi')
+
+@section('content')
 <h1>program studi</h1>
 <ul>
 @foreach ($viewlist as $item)
@@ -11,3 +16,33 @@
     <li>{{$item ["npm"] }} - {{$item ["nama"] }}</li>
 @endforeach
 </ul>
+<div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Program Studi</h4>
+                  <p class="card-description">
+                    List data Program studi
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Nama Program Studi</th>
+                          <th>Singkatan</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach ($fakultas as $item)
+                        <tr>
+                            @foreach ($viewlist as $item)
+                            <th>{{$item}}</th>
+                            @endforeach
+                            </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+@endsection
--- laravelsi4b/example-app/resources/views/prodi/index.blade.php
+++ laravelsi4b/example-app/resources/views/prodi/index.blade.php
@@ -1,18 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Program studi</title>
-</head>
-<body>
-    <h1>UMDP</h1>
-    <h2>Program studi</h2>
-    <ul>
-        @foreach ($prodi as $item)
-            <li>{{$item["nama"]}} {{$item["singkatan"]}} {{$item["fakultas"]["nama"]}}</li>
-        @endforeach
-    </ul>
-</body>
-</html>
+
+@extends('layout.main')
+
+@section('title','prodi')
+
+@section('content')
+<div class="row">
+<div class="col-lg-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Program Studi</h4>
+                  <p class="card-description">
+                    List data Program studi
+                  </p>
+                  <div class="table-responsive">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>Nama Program Studi</th>
+                          <th>Singkatan</th>
+                          <th>Nama Fakultas</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach ($prodi as $item)
+                        <tr>
+                            <td>{{$item["nama"]}}</td>
+                            <td>{{$item["singkatan"]}}</td>
+                            <td>{{$item["fakultas"]["nama"]}}</td>
+                            </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+</div>
+@endsection
